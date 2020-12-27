939527c2c245b95c08706819c074b649d4c962d1
Luis Martinez <luis_mmartinez@live.com>
2020-12-27 22:43:06 +0000

    Done.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+
+
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    array.collect do |element|
+      array.join("#{array.index(element)}")
+    end
+  end
+end
+
+
+
+
 # Deli Counter - Take a Number
 
 ## Objectives
--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require "pry"
+
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    new_array= array.collect! {|element| "#{array.index(element)+1}. " "#{element}"}
+    string_array= new_array.join(" ")
+    puts "The line is currently: #{string_array}"
+  end
+end
+
+
+def take_a_number(array,name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    first_inline= array.shift
+    puts "Currently serving #{first_inline}."
+  end
+end
